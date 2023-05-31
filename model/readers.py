7e352fb8dd70b430ff271afdd70c3cd54b53c934
disvs <sbsommers3@gmail.com>
2023-05-31 15:04:40 -0700

    modified:   model/readers.py

--- model/readers.py
+++ model/readers.py
@@ -145,7 +145,7 @@ def initReaders():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        r1 = Readers( username="ssdebate", name="Shreya", book ="The Hunger Games", finishedate=2019, rating=5)
+        r1 = Readers( username="ssdebates", name="Shreya", book ="The Hunger Games", finishedate=2019, rating=5)
         r2 = Readers( username="jdiver", name="Jiya", book ="Divergent", finishedate=2019, rating=3)
         r3 = Readers( username="vaf", name="Vaishavi", book ="Animal Farm", finishedate=2019, rating=4)
 
